a002b11e3ac10aabc29d844ed6c3ef2aea94ca61
rostyslav <rostik@telelan.com.ua>
2018-04-08 20:19:32 +0200

    carousel and touch option

--- functions/index.js
+++ functions/index.js
@@ -5,39 +5,137 @@ const App = require('actions-on-google').DialogflowApp;
 const functions = require('firebase-functions');
 
 
-// a. the action name from the make_name Dialogflow intent
-const WELCOME_INTENT = 'input.welcome';  // the action name from the Dialogflow intent
-const NAME_ACTION = 'create_name';
+const WELCOME_INTENT = 'input.welcome';
+const USERNAME_ACTION = 'user.name';
+const DEFAULT_FALLBACK = 'input.unknown';
+const NO_INPUT_EVENT = 'no.input';
+const HERO_SELECTION = 'hero.touch';
 
-// b. the parameters that are parsed from the make_name intent
-const NAME_ARGUMENT = 'given-name';
+const HERO_NAME_CONTEXT = 'hero.name';
+
+const IMAGES = {
+    hero1: { url: 'https://firebasestorage.googleapis.com/v0/b/rock-paper-scissors-d9968.appspot.com/o/wait.png?alt=media&token=2816274a-e19d-4aca-8d21-b14fedb14813', heroName: 'Cosmic Bug'},
+    hero2: { url: 'https://firebasestorage.googleapis.com/v0/b/rock-paper-scissors-d9968.appspot.com/o/rsz_minions.jpg?alt=media&token=afb4713b-3efc-439e-a61a-d47d0058b0a1', heroName: 'Fluffy Worm'}
+};
+
+const HERONAME_ARGUMENT = 'hero';
+
+// b. the parameters that are parsed from the create_name intent
+const USERNAME_ARGUMENT = 'given-name';
 
 
 // TODO: add surface capabilities(do you have screen?)
 
 exports.rockPaperScissors = functions.https.onRequest((request, response) => {
     const app = new App({request, response});
-    console.log('Request headers: ' + JSON.stringify(request.headers));
-    console.log('Request body: ' + JSON.stringify(request.body));
+    app.data.fallbackCount = 0;
+    // console.log('Request headers: ' + JSON.stringify(request.headers));
+    // console.log('Request body: ' + JSON.stringify(request.body));
 
 
     function welcomeIntentQuestion (app) {
-        app.ask(`Hi User! What is your name?`,
-            ['I didn\'t hear a number', 'If you\'re still there, what\'s your lucky number?',
+        app.ask(`Hi Stranger. You've made great choice. My heroes are waiting to play with You! You will play up to three wins. But first they want to know: what is Your name?`,
+            ['I didn\'t hear a name', 'If you\'re still there, what\'s your lucky number?',
                 'We can stop here. Let\'s play again soon. Bye!']);
     }
 
     function createName (app) {
-        let name = app.getArgument(NAME_ARGUMENT);
+        const username = app.getArgument(USERNAME_ARGUMENT);
 
-        app.ask(`Alright! Your super silly name is "${name}"! I hope you like it. Which kind of sport do you like to play?`);
+        app.askWithCarousel(app.buildRichResponse()
+            .addSimpleResponse(`Ok, ${username}, now choose with whom You want to play. Cosmic Bug and Fluffy Worm are waiting for You. Play carefully with them, they are very cunning!`)
+            .addSuggestions([IMAGES.hero1.heroName, IMAGES.hero2.heroName]),
+            app.buildCarousel()
+                .addItems(app.buildOptionItem('COSMIC_BUG',
+                    ['cosmic', 'bug', 'buggy', 'cosmic bug'])
+                    .setTitle(IMAGES.hero1.heroName)
+                    // .setDescription('This is a description of a carousel item')
+                    .setImage(IMAGES.hero1.url, IMAGES.hero1.heroName))
+                .addItems(app.buildOptionItem('FLUFFY_WORM',
+                    ['fluffy', 'worm', 'fluffy worm'])
+                    .setTitle(IMAGES.hero2.heroName)
+                    // .setDescription('Google Home is a voice-activated speaker powered ')
+                    .setImage(IMAGES.hero2.url, IMAGES.hero2.heroName)
+                )
+            );
+    }
+
+    function heroSelection (app) {
+        const param = app.getSelectedOption();
+        const username = app.getArgument(USERNAME_ARGUMENT);
+
+        if (!param) {
+            app.ask('You did not select any hero from the list or carousel');
+        } else if (param === 'COSMIC_BUG') {
+            app.ask(`Aha, ${username}. Let's see who has sharper mind! I trained a lot! So, what is your shoot?`);
+        } else if (param === 'FLUFFY_WORM') {
+            app.ask(`Hi ${username}, lets play with you. To be continued.`);
+        } else {
+            app.ask('You selected an unknown item from the list or carousel');
+        }
+    }
+
+    function noInput (app) {
+        if (app.getRepromptCount() === 0) {
+            app.ask(`What was that?`);
+        } else if (app.getRepromptCount() === 1) {
+            app.ask(`Sorry I didn't catch that. Could you repeat yourself?`);
+        } else if (app.isFinalReprompt()) {
+            app.tell(`Okay let's try this again later.`);
+        }
+    }
+
+    function defaultFallback (app) {
+        // console.log('!defaultFallback data-------->', app.data);
+        // console.log('app.getRepromptCount()', app.getRepromptCount());
+        // app.data.fallbackCount++;
+
+        app.ask(`I missed what you said. Say it again?`);
+        if (app.data.fallbackCount === 2) {
+            app.tell('Hmm, since I\'m still having trouble, so I\'ll stop here. Let’s play again soon.');
+        }
     }
 
 
     let actionMap = new Map();
 
+    // actionMap.set(app.StandardIntents.OPTION, () => {
+    //     const param = app.getSelectedOption();
+    //     if (!param) {
+    //         app.ask('You did not select any hero from the list or carousel');
+    //     } else if (param === 'COSMIC_BUG') {
+    //         app.ask('Aha, User. Let"s see who has sharper mind! I trained a lot! So, what is your shoot?');
+    //     } else if (param === 'FLUFFY_WORM') {
+    //         app.ask('Hi User, lets play with you. To be continued.');
+    //     } else {
+    //         app.ask('You selected an unknown item from the list or carousel');
+    //     }
+    // });
+
     actionMap.set(WELCOME_INTENT, welcomeIntentQuestion);
-    actionMap.set(NAME_ACTION, createName);
+    actionMap.set(USERNAME_ACTION, createName);
+    actionMap.set(HERO_SELECTION, heroSelection);
+
+    actionMap.set(DEFAULT_FALLBACK, defaultFallback);
+    actionMap.set(NO_INPUT_EVENT, noInput);
 
     app.handleRequest(actionMap);
 });
+
+
+
+
+
+// `
+// I missed what you said. Say it again?
+// Sorry, could you say that again?
+// Sorry, can you say that again?
+// Can you say that again?
+// Sorry, I didn't get that.
+// Sorry, what was that?
+// One more time?
+// What was that?
+// Say that again?
+// I didn't get that.
+// I missed that.
+
