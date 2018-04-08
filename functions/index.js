3e861c4ce7890f65e87e5dd3bb4b89e75424b144
rostyslav <rostik@telelan.com.ua>
2018-04-08 21:17:19 +0200

    set Context of user name

--- functions/index.js
+++ functions/index.js
@@ -11,24 +11,39 @@ const DEFAULT_FALLBACK = 'input.unknown';
 const NO_INPUT_EVENT = 'no.input';
 const HERO_SELECTION = 'hero.touch';
 
-const HERO_NAME_CONTEXT = 'hero.name';
 
 const IMAGES = {
     hero1: { url: 'https://firebasestorage.googleapis.com/v0/b/rock-paper-scissors-d9968.appspot.com/o/wait.png?alt=media&token=2816274a-e19d-4aca-8d21-b14fedb14813', heroName: 'Cosmic Bug'},
     hero2: { url: 'https://firebasestorage.googleapis.com/v0/b/rock-paper-scissors-d9968.appspot.com/o/rsz_minions.jpg?alt=media&token=afb4713b-3efc-439e-a61a-d47d0058b0a1', heroName: 'Fluffy Worm'}
 };
 
-const HERONAME_ARGUMENT = 'hero';
+const FALLBACK_PHRASES = [
+    `I missed what you said. Say it again?`,
+    'Sorry, could you say that again?',
+    'Sorry, can you say that again?',
+    'Can you say that again?',
+    `Sorry, I didn't get that.`,
+    'Sorry, what was that?',
+    'One more time?',
+    'What was that?',
+    'Say that again?',
+    `I didn't get that.`,
+    'I missed that.'
+];
 
-// b. the parameters that are parsed from the create_name intent
+const USER_NAME_CONTEXT = 'username_context';
 const USERNAME_ARGUMENT = 'given-name';
+
+function getRandomPhrase(arr) {
+    return arr[Math.floor(Math.random()*arr.length)];
+}
 
 
 // TODO: add surface capabilities(do you have screen?)
 
 exports.rockPaperScissors = functions.https.onRequest((request, response) => {
     const app = new App({request, response});
-    app.data.fallbackCount = 0;
+    // app.data.fallbackCount = 0;
     // console.log('Request headers: ' + JSON.stringify(request.headers));
     // console.log('Request body: ' + JSON.stringify(request.body));
 
@@ -42,6 +57,10 @@ exports.rockPaperScissors = functions.https.onRequest((request, response) => {
     function createName (app) {
         const username = app.getArgument(USERNAME_ARGUMENT);
 
+        const parameters = {};
+        parameters[USERNAME_ARGUMENT] = username;
+        app.setContext(USER_NAME_CONTEXT, 100, parameters);
+
         app.askWithCarousel(app.buildRichResponse()
             .addSimpleResponse(`Ok, ${username}, now choose with whom You want to play. Cosmic Bug and Fluffy Worm are waiting for You. Play carefully with them, they are very cunning!`)
             .addSuggestions([IMAGES.hero1.heroName, IMAGES.hero2.heroName]),
@@ -49,12 +68,10 @@ exports.rockPaperScissors = functions.https.onRequest((request, response) => {
                 .addItems(app.buildOptionItem('COSMIC_BUG',
                     ['cosmic', 'bug', 'buggy', 'cosmic bug'])
                     .setTitle(IMAGES.hero1.heroName)
-                    // .setDescription('This is a description of a carousel item')
                     .setImage(IMAGES.hero1.url, IMAGES.hero1.heroName))
                 .addItems(app.buildOptionItem('FLUFFY_WORM',
                     ['fluffy', 'worm', 'fluffy worm'])
                     .setTitle(IMAGES.hero2.heroName)
-                    // .setDescription('Google Home is a voice-activated speaker powered ')
                     .setImage(IMAGES.hero2.url, IMAGES.hero2.heroName)
                 )
             );
@@ -62,7 +79,7 @@ exports.rockPaperScissors = functions.https.onRequest((request, response) => {
 
     function heroSelection (app) {
         const param = app.getSelectedOption();
-        const username = app.getArgument(USERNAME_ARGUMENT);
+        const username = app.getContextArgument(USER_NAME_CONTEXT, USERNAME_ARGUMENT).value;
 
         if (!param) {
             app.ask('You did not select any hero from the list or carousel');
@@ -90,7 +107,8 @@ exports.rockPaperScissors = functions.https.onRequest((request, response) => {
         // console.log('app.getRepromptCount()', app.getRepromptCount());
         // app.data.fallbackCount++;
 
-        app.ask(`I missed what you said. Say it again?`);
+        app.ask(getRandomPhrase(FALLBACK_PHRASES));
+
         if (app.data.fallbackCount === 2) {
             app.tell('Hmm, since I\'m still having trouble, so I\'ll stop here. Let’s play again soon.');
         }
@@ -98,20 +116,6 @@ exports.rockPaperScissors = functions.https.onRequest((request, response) => {
 
 
     let actionMap = new Map();
-
-    // actionMap.set(app.StandardIntents.OPTION, () => {
-    //     const param = app.getSelectedOption();
-    //     if (!param) {
-    //         app.ask('You did not select any hero from the list or carousel');
-    //     } else if (param === 'COSMIC_BUG') {
-    //         app.ask('Aha, User. Let"s see who has sharper mind! I trained a lot! So, what is your shoot?');
-    //     } else if (param === 'FLUFFY_WORM') {
-    //         app.ask('Hi User, lets play with you. To be continued.');
-    //     } else {
-    //         app.ask('You selected an unknown item from the list or carousel');
-    //     }
-    // });
-
     actionMap.set(WELCOME_INTENT, welcomeIntentQuestion);
     actionMap.set(USERNAME_ACTION, createName);
     actionMap.set(HERO_SELECTION, heroSelection);
@@ -121,21 +125,4 @@ exports.rockPaperScissors = functions.https.onRequest((request, response) => {
 
     app.handleRequest(actionMap);
 });
-
-
-
-
-
-// `
-// I missed what you said. Say it again?
-// Sorry, could you say that again?
-// Sorry, can you say that again?
-// Can you say that again?
-// Sorry, I didn't get that.
-// Sorry, what was that?
-// One more time?
-// What was that?
-// Say that again?
-// I didn't get that.
-// I missed that.
 
